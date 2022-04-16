137c2798e36901e8f1f101579a8d8c6cc2007d41
Guhan M <mguhan439@gmail.com>
2022-04-16 20:15:53 +0530

    added app.yaml

--- index.js
+++ index.js
@@ -135,7 +135,8 @@ app.get('/transaction/all', async (req, res) => {
   res.status(200).json(transactions);
 });
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
